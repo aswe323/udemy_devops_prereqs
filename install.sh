3729022bc21ce53be73d340a3f4ed41786622b6c
Your Name <you@example.com>
2024-09-12 11:51:38 +0300

    changes to enable installation of maven

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #technically requires virtualbox
-apt-get install openjdk-8-jdk maven awscli curl -y
+apt update
+apt-get install openjdk-8-jdk maven curl software-properties-common -y
+apt-add-repository universe -y
+apt update -y
+apt install maven -y
+#awscli doesn't exist in repo, we use snap insted
+#
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-apt install ./vagrant_2.2.9_x86_64.deb
+sudo apt install ./vagrant_2.2.9_x86_64.deb
 # redundent 
 # apt install git  -y
 snap install intellij-idea-community --classic -y
+snap install aws-cli --classic
 apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 add-apt-repository "deb https://download.sublimetext.com/ apt/stable/" -y
