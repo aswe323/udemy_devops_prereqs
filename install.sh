06244e3015bdb9fe47bf31e189d369eaf3fefb1c
Your Name <you@example.com>
2024-09-12 11:35:37 +0300

    changes to batch install:w

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
 apt install ./vagrant_2.2.9_x86_64.deb
 apt install git  -y
-apt-get install openjdk-8-jdk -y
-apt-get install maven -y 
-apt-get install awscli -y
+apt-get install openjdk-8-jdk maven awscli -y
 snap install intellij-idea-community --classic
 apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
