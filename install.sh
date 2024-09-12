fa4320b6e2ff410e0d5c4c9ced9b44511c7cb71a
Your Name <you@example.com>
2024-09-12 11:38:39 +0300

    further changes, install curl ahead of use

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #technically requires virtualbox
+apt-get install openjdk-8-jdk maven awscli curl -y
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
 apt install ./vagrant_2.2.9_x86_64.deb
-apt install git  -y
-apt-get install openjdk-8-jdk maven awscli -y
-snap install intellij-idea-community --classic
-apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+# redundent 
+# apt install git  -y
+snap install intellij-idea-community --classic -y
+apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+add-apt-repository "deb https://download.sublimetext.com/ apt/stable/" -y
 apt install sublime-text -y
